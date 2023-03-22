b75f41538f3c4a5d2c08d5e0ebea3e3e9cd40d2e
jaypersanchez <jaypersanchez@gmail.com>
2023-03-23 05:00:09 +0800

    routes

--- routes/crypto.js
+++ routes/crypto.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Crypto Currencies');
+  axios.get(`${process.env.NEWS_BASE_URL}?country=us&apiKey=${process.env.NEWS_API}`)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'Error fetching data' });
+    });
+  //res.send('Crypto Currencies');
 });
 
 module.exports = router;
--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config()
+const axios = require('axios');
 
-/* GET users listing. */
+/* GET users listing. Default US news headlines */
 router.get('/', function(req, res, next) {
-  res.send('News Headlines');
+
+  axios.get(`${process.env.NEWS_BASE_URL}?country=us&apiKey=${process.env.NEWS_API}`)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'Error fetching data' });
+    });
+  //res.send('News Headlines');
 });
 
 module.exports = router;
