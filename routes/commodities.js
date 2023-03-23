a5bdf457056b5e6b74ec9753857f5c722792d913
jaypersanchez <jaypersanchez@gmail.com>
2023-03-23 13:46:31 +0800

    rate per pair endpoint

--- routes/commodities.js
+++ routes/commodities.js
@@ -16,4 +16,28 @@ router.get('/', function(req, res, next) {
   //res.send('Commodities');
 });
 
+router.get('/pairs', function(req, res, next) {
+  axios.get(`${process.env.FOREX_BASE_URL}`)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'Error fetching data' });
+    });
+  //res.send('Commodities');
+});
+
+router.get('/rate', function(req, res, next) {
+  axios.get(`${process.env.FOREX_BASE_URL}?pairs=USDCAD`)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'Error fetching data' });
+    });
+  //res.send('Commodities');
+});
+
 module.exports = router;
